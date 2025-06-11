03de2ae7ae8ea682aa52163311b3a36dd2179df1
goodmorninggoodbye <110742252+goodmorninggoodbye@users.noreply.github.com>
2025-06-11 18:06:34 -0400

    Update thread-worker.h

--- thread-worker.h
+++ thread-worker.h
@@ -1,19 +1,8 @@
-// File:	worker_t.h
-
-// List all group member's name:
-/*
- * Ewan Jee (ej307)
- * Paul Samoylov (ps1172)
-*/
-// username of iLab: kill
-// iLab Server: kill.cs.rutgers.edu
-
 #ifndef WORKER_T_H
 #define WORKER_T_H
 
 #define _GNU_SOURCE
 
-/* include lib header files that you need here: */
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <stdio.h>
@@ -21,7 +10,6 @@
 #include <ucontext.h>
 #include "mutex_types.h"
 
-/* Function Declarations: */
 
 /* create a new thread */
 int worker_create(worker_t *thread, pthread_attr_t *attr, void *(*function)(void *), void *arg);
@@ -36,8 +24,7 @@ void worker_exit(void *value_ptr);
 int worker_join(worker_t thread, void **value_ptr);
 
 /* initial the mutex lock */
-int worker_mutex_init(worker_mutex_t *mutex, const pthread_mutexattr_t
-												 *mutexattr);
+int worker_mutex_init(worker_mutex_t *mutex, const pthread_mutexattr_t *mutexattr);
 
 /* aquire the mutex lock */
 int worker_mutex_lock(worker_mutex_t *mutex);
